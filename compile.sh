b3067ce662610561c079f8f3352446f2a91320af
voxel <voxel@andreev.msk.ru>
2022-04-08 09:42:00 +0300

    Version 9.2.5.2.30SF-HW

--- compile.sh
+++ compile.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 export TZ=UTC
-PATH=/home/voxel/local/bin:/usr/local/bin:/usr/bin:/bin:/usr/local/games:/usr/games
-/usr/bin/make -j8 tools/install V=s >& reptools
+PATH=/home/voxel/local/bin:/home/voxel/go/bin:/usr/local/bin:/usr/bin:/bin:/usr/local/games:/usr/games
+/usr/bin/make tools/install V=s >& reptools
 make -j4 toolchain/install V=s >& reptoolchain
 make V=99 >& report
